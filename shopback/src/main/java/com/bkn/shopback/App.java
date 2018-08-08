266f0555c5b249ec820fff2c9e9ceb157867b613
Balaji <balajibala.bb66@gmail.com>
2018-08-08 18:28:33 +0530

    My Project Setup Completed Successfully

---
+++ shopback/src/main/java/com/bkn/shopback/App.java
@@ -0,0 +1,13 @@
+package com.bkn.shopback;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
